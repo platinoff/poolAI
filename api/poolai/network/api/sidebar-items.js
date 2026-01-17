ae54e043fbe1387042f2c501110f66a6b40fc5a3
platinoff <platinoff@users.noreply.github.com>
2026-01-17 04:47:54 +0000

    deploy: cce5879efe8d393e32bbfe3f1a5d479e82800d80

--- api/poolai/network/api/sidebar-items.js
+++ api/poolai/network/api/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["create_api_routes"],"mod":["common","libraries","raid","rewards","system","users","vm","workers"]};
+window.SIDEBAR_ITEMS = {"fn":["create_api_routes"],"mod":["common","discovery","libraries","raid","rewards","system","users","vm","workers"]};
---
+++ api/poolai/network/discovery/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DiscoveryMessage"],"struct":["DiscoveryConfig","DiscoveryService","PeerCapabilities","PeerInfo"]};
