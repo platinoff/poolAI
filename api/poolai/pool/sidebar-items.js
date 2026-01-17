d4fba344af4cda70e0614890e64a4d5e4c8ae367
platinoff <platinoff@users.noreply.github.com>
2026-01-17 04:54:40 +0000

    deploy: cd1204b511975137ed3390dd793c83eec5c17d5a

--- api/poolai/pool/sidebar-items.js
+++ api/poolai/pool/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["LoadBalancingStrategy"],"fn":["get_global_pool","health_check","initialize","initialize_with_config","shutdown"],"mod":["worker"],"struct":["Pool","PoolConfig","PoolMetrics"]};
+window.SIDEBAR_ITEMS = {"enum":["LoadBalancingStrategy"],"fn":["get_global_pool","health_check","initialize","initialize_with_config","shutdown"],"mod":["discovery_integration","worker"],"struct":["Pool","PoolConfig","PoolMetrics"]};
