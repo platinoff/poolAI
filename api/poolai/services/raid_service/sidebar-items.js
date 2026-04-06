0d8b7978060b4a8861a82806bb1b75e21ce052cc
platinoff <platinoff@users.noreply.github.com>
2026-04-06 00:54:21 +0000

    deploy: e3316c3162c8d7af991248ef54619749bbd05b70

--- api/poolai/services/raid_service/sidebar-items.js
+++ api/poolai/services/raid_service/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["RAID_MANAGER_UNAVAILABLE_MESSAGE"],"enum":["RaidServiceError"],"struct":["RaidService"]};
+window.SIDEBAR_ITEMS = {"constant":["RAID_MANAGER_UNAVAILABLE_MESSAGE"],"enum":["RaidServiceError"],"struct":["RaftStatus","RaidQuotaResponse","RaidService","RaidStatusResponse","RaidStorageStatus"]};
